923d748f1538aea2e0ba2dd3254d2a96f9334fb7
Brian Olencki <brian@olencki.com>
2021-02-21 23:21:45 -0500

    [API] Add support for DB uri instead of construction

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,21 +3,10 @@ import Database from './services/Database'
 
 dotenv.config()
 
-const address = process.env.DB_ADDRESS
-if (!address) {
-  throw new Error('DB_ADDRESS is not set')
+const uri = process.env.DB_URI
+if (!uri) {
+  throw new Error('DB_URI is not set in env.')
 }
-const name = process.env.DB_NAME
-if (!name) {
-  throw new Error('DB_ADDRESS is not set')
-}
-
-const uri = Database.parseUri({
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  address,
-  name
-})
 const db = new Database(uri)
 
 import { app } from './http/server' // intentionally imported here due to service container restraints
